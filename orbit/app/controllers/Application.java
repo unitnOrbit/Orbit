d905fddf4a7e616ce69ea8dd897c56e47792e62e
Alektos <luigipietrobelli@gmail.com>
2012-04-03 13:30:29 +0200

    Fix: css -> menubar

--- orbit/app/controllers/Application.java
+++ orbit/app/controllers/Application.java
@@ -9,7 +9,6 @@ public class Application extends Controller {
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
-
    
   public static Result prova() {
       return TODO;
