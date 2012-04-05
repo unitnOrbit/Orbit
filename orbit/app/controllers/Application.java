556edd206d0f7351acfaebff5e4d4b091b28d267
Davide Kirchner <davide.kirchner@yahoo.it>
2012-04-05 10:26:37 +0200

    primo model parzialmente funzionante

--- orbit/app/controllers/Application.java
+++ orbit/app/controllers/Application.java
@@ -3,19 +3,32 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import views.html.*;
+import models.Country;
+import java.util.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-	return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is ready."));
     }
    
     public static Result prova() {
-	return TODO;
+        return TODO;
     }
   
     public static Result dbtest() {
-	return TODO;
+        //Country c = new Country();
+	//c.name = "Vaffanculo";
+        //c.region = "Northen Europe";
+        //c.save();
+
+        List<Country> l = Country.find.all();
+        StringBuffer ret_val = new StringBuffer();
+	for (Country c2: l){
+	    ret_val.append(c2.toString());
+        }
+        
+        return ok(ret_val.toString());
     }
   
 }
