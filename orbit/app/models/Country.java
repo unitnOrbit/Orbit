3b6da08df02a20d029e02da0f6f072d9e5404ffe
Davide Kirchner <davide.kirchner@yahoo.it>
2012-04-07 11:04:58 +0200

    Importazione models autogenerati

--- orbit/app/models/Country.java
+++ orbit/app/models/Country.java
@@ -2,41 +2,54 @@ package models;
 
 import java.util.*;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import play.db.ebean.*;
 import play.data.format.*;
 import play.data.validation.*;
 
-@Entity 
-@Table(name="countries")
+@Entity
+@Table(name = "countries")
 public class Country extends Model {
-
+    public static final long serialVersionUID = 1L;
     @Id
-    @Column(name="country_ID")
-    public Long id;
-
-    //@Constraints.Required
-    @Constraints.MaxLength(255)
-    @Column(length=255, unique=true)
+    @NotNull
+    @Column(name = "country_ID")
+    public Integer countryID;
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "name")
     public String name;
-
-    //@Constraints.Required
-    @Constraints.MaxLength(200)
-    @Column(length=200)
+    @Size(min = 1, max = 200)
+    @Column(name = "region")
     public String region;
-
-    //@Constraints.Required
-    @Constraints.MaxLength(255)
+    @Size(min = 1, max = 255)
+    @Column(name = "citizenship")
     public String citizenship;
-
-    //@Constraints.Required
+    @Column(name = "deleted")
     public boolean deleted;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "countryOfProvenance")
+    public Collection<Student> studentsCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "citizenship")
+    public Collection<Student> studentsCollection1;
+    @OneToMany(mappedBy = "country")
+    public Collection<University> universitiesCollection;
 
-    public static Finder<Long,Country> find = new Finder<Long,Country>(
-        Long.class, Country.class
-    ); 
 
-    public String toString(){
-        return "Country instance: <" + name + ">";
+    public static Finder<Long,Country> find = new Finder(
+      Long.class, Country.class
+    );
+
+    public static List<Country> all() {
+      return find.all();
     }
+  
+    public static void create(Country country) {
+      country.save();
+    }
+
+    public static void delete(Long id) {
+      find.ref(id).delete();
+    }    
 }
