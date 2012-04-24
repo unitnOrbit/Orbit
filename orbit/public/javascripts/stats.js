06d925089c1cfae7b0361d618b2cf8c7b5a533a1
Luigi Pietrobelli <alektos@AlektosMac.local>
2012-04-24 13:36:13 +0200

    aggiunto modal box per messaggi

--- orbit/public/javascripts/stats.js
+++ orbit/public/javascripts/stats.js
@@ -1,3 +1,53 @@
+function createLoadingMsg(title, msg) {
+	    $('body').prepend('
+<div class="modal" id="load_div">
+  <div class="modal-header">
+    <h3>Loading... </h3>
+  </div>
+  <div class="modal-body">
+    <p>Please wait... </p>
+  </div>
+  <div class="modal-footer">
+    <a href="#" class="btn">Close</a>
+    <a href="#" class="btn btn-primary">Save changes</a>
+  </div>
+</div>');
+
+}
+
+
+/**
+   Ritorna la lista dei grafici appartenenti alla categoria selezionata
+   elem: id elemento o classe in cui stampare le stringhe
+*/
+function getListOfCharts(category_id, main_container) {
+    var url_chart_list = "";
+
+    $.ajax({
+        url: url_chart_list,
+        dataType: 'json',
+        data: { cat_id: category_id }, // vuoto per il momento
+	beforeSend: function() {
+	    // test loading
+	    
+	    $('#myModal').modal('show');
+	},
+        success: function(data) {
+            $.each(data['list'], function(index, val) {
+		$("#" + main_container).append(
+		    $("#" + main_container).children().clone().css("display", "block").each(
+			function(k, val) {
+			    $(this).find("h2").text(val);
+			}
+		    ));		
+            });		    
+        },
+        error: function() {},
+        complete: function() {}
+    });    
+}
+
+
 /**
    Get List of main categories
 */
@@ -7,16 +57,12 @@ function getCategoryList() {
     var msgSuccess = "<b>Success from '" + url_category_list + "': </b>";
     var msgComplete = "<b>Complete: </b>eseguo altro. ";
 
-
     // test con: $.ajax
     $.ajax({
         url: url_category_list,
         dataType: 'json',
         data: '', // vuoto per il momento
-        success: function(data) {
-            //$("#test_ajax").append('<div class="alert alert-success">' + 
-            //			   msgSuccess + '<ul id="testList">');
-		    
+        success: function(data) {		    
             $.each(data['list'], function(index, val) {
                 $("#cat_list_container").append( 
                     $("#cat_item_template").clone().css('display','block').each(function (i,elem) {
