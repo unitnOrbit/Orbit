8e33f9f192debb308d930bef2869fb15cdbfd72c
DamianFox <d.fossa.91@gmail.com>
2012-05-24 13:40:24 +0200

    modifica delle categorie, reports e statistiche (ancora provvisorio)

--- orbit/conf/evolutions/default/2.sql
+++ orbit/conf/evolutions/default/2.sql
@@ -78,43 +78,47 @@ INSERT INTO `widget` (`id`, `name`, `description`) VALUES
 
 
 INSERT INTO `report` (`id`, `name`, `description`) VALUES
-(1, 'Distribution Of 1st Year Admitted Students By Nationality', 'distribution of 1st year admitted'),
-(2, 'Distribution Of Current Students By Nationality', 'distribution of current students'),
-(3, 'Average Mark In Courses By Funding Institution', 'number of months spent abroad'),
-(4, 'Number Of Application', 'applications');
+(1, 'Distribution of first year admitted students by nationality for the current year ', 'distribution of first year admitted students by nationality'),
+(2, 'Distribution of 1st year admitted students by nationality, over the last 5 years ', 'distribution of 1st year admitted students by nationality, over the last 5 years '),
+(3, 'Distribution of current students by nationality ', 'distribution of current students by nationality '),
+(4, 'Number of scholarships over the last 5 years, classified by source (UNITN, DISI, Research center, Company, Other) ', 'number of scholarships over the last 5 years, classified by source '),
+(5, 'Average number of months spent abroad, by cycle, historically (for 5 cycles) ', 'average number of months spent abroad, by cycle, historically'),
+(6, 'Average number of months spent abroad, by cycle and by funding institution, historically (for 5 cycles) ', 'average number of months spent abroad, by cycle and by funding institution, historically'),
+(7, 'Average mark in courses by funding institution, over the past 5 years', 'average mark in courses by funding institution, over the past 5 years');
 
 
 
 INSERT INTO `category` (`id`, `name`, `description`) VALUES
 (1, 'Current Students', 'Statistic reports about students'' marks, for current students'),
-(2, 'Student applications', 'Statistic reports about student applications over the last years'),
-(3, 'Student marks', 'Students marks');
+(2, 'Student Scholarships', 'Statistic reports about student scholarships over the last 5 years'),
+(3, 'Student Marks', 'Students marks'),
+(4, 'Time Spent Abroad', 'Number of Month Spent Abroad');
 
 
 INSERT INTO `statistic` (`id`, `name`, `description`, `format`, `num_visits`, `widget_id`, `dataset_id`) VALUES
-(1, 'distribution of 1st year admitted students by nationality for the current year ', 'The output should be the list of countries, each one with the % of students from that country admitted to the 1st year ', NULL, NULL, 1, 2),
-(2, 'distribution of current students by nationality ', 'The output should be the list of countries, each one with the % of students from that country currently enrolled ', NULL, NULL, 1, 2),
-(3, 'distribution of 1st year admitted students by nationality, over the last 5 years ', 'The output should be the list of countries, each one with the % of students from that country admitted to the 1st year, over the last 5 years ', NULL, NULL, 1, 2),
-(4, 'Number of applications', 'Number of applications over the last 5 years', NULL, NULL, 1, 1),
-(5, 'number of scholarships over the last 5 years, classified by source (UNITN, DISI, Research center, Company, Other) ', 'area chart', NULL, NULL, 1, 3),
-(6, 'avg number of months spent abroad, by cycle, historically (for 5 cycles) ', 'Bar chart', NULL, NULL, 1, 4),
-(7, 'avg number of months spent abroad, by cycle and by funding institution, historically (for 5 cycles) ', 'Area chart', NULL, NULL, 1, 3),
-(8, 'avg mark in courses by funding institution, over the past 5 yrs. Possibility to focus (select) specific institutions ', 'clustered bar chart', NULL, NULL, 1, 4);
+(1, 'Distribution of 1st year admitted students by nationality for the current year ', 'The graph shows the list of countries, each one with the percentage of students from that country admitted to the 1st year ', NULL, NULL, 1, 2),
+(2, 'Distribution of current students by nationality ', 'The graph shows the list of countries, each one with the percentage of students from that country currently enrolled ', NULL, NULL, 1, 2),
+(3, 'Distribution of 1st year admitted students by nationality, over the last 5 years ', 'The graphs shows the list of countries, each one with the percentage of students from that country admitted over the last 5 years ', NULL, NULL, 1, 2),
+(4, 'Number of applications over the last 5 years', 'The graph shows the number of applications over the last 5 years', NULL, NULL, 1, 1),
+(5, 'Number of scholarships over the last 5 years, ', 'The graph shows the number of scholarships over the last 5 years, classified by source (UNITN, DISI, Research center, Company, Other) ', NULL, NULL, 1, 3),
+(6, 'Average number of months spent abroad, by cycle, historically (for 5 cycles) ', 'The graph shows the average number of months spent abroad by cycle, fo 5 cycles', NULL, NULL, 1, 4),
+(7, 'Average number of months spent abroad, by cycle and by funding institution, historically (for 5 cycles) ', 'The graph shows the average number of months spent abroad by cycles and by funding institution, for 5 years', NULL, NULL, 1, 3),
+(8, 'Average mark in courses by funding institution, over the past 5 years', 'The graph shows the average mark in courses by funding institution, over the past 5 years', NULL, NULL, 1, 4);
 
 
 INSERT INTO `report_category` (`report_id`, `category_id`) VALUES
 (1, 1),
 (2, 1),
-(3, 3),
-(4, 2);
+(3, 1),
+(4, 2),
+(7, 3);
 
 
 INSERT INTO `report_statistic` (`report_id`, `statistic_id`) VALUES
 (1, 1),
-(1, 2),
 (2, 2),
-(3, 8),
-(4, 4);
+(3, 1),
+(4, 5);
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
