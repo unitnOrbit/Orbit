57c4904c05ee264aff7e3e5dccbc01bbc037890b
Davide Kirchner <davide.kirchner@yahoo.it>
2012-05-29 19:22:52 +0200

    ScholarshipOver5bySource built

--- orbit/app/datasets/ScholarshipOver5bySource.java
+++ orbit/app/datasets/ScholarshipOver5bySource.java
@@ -5,16 +5,21 @@ import models.global.*;
 
 import play.db.ebean.Model;
 import javax.persistence.Entity;
-//import javax.persistence.OneToOne;  
-//import com.avaje.ebean.annotation.Sql;  
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.RawSqlBuilder;
 
 /**
- * this class represents the number scholarship over the last 5 years, classified by source
+ * DataSet for the number scholarship over
+ * the last 5 years, classified by Funding Institution
+ *
+ * Builds the data table as follows:
+ *   Year   | Fund.Inst1 | ... | Fund.InstN
+ * [string] | [number]   | ... | [number]
+ * ---------+------------+-----+----------
+ * 2011-2012|   num      | ... |   num
  */
 public class ScholarshipOver5bySource implements DataSet {
-    
+
     public static final int YEARS_REQUIRED = 5;
 
     private List<FundingInstitution> institutions;
@@ -25,7 +30,10 @@ public class ScholarshipOver5bySource implements DataSet {
             institutions.add(fi);
         }
     }
-	
+
+    /**
+     * No options supported
+     */
     public void setOptions(Map options) {
 	// no option supported yet
     }
@@ -35,40 +43,37 @@ public class ScholarshipOver5bySource implements DataSet {
 	List<String> col;
 	col = new LinkedList<String>();
 	col.add(DataSet.ColTypes.STRING);
-	col.add("FundingInstitutions");
+	col.add("Academic Years");
 	cols.add(col);
-	
-                for (FundingInstitution institutions: this.institutions) {    
+
+        for (FundingInstitution institutions: this.institutions) {
 	     col = new LinkedList<String>();
 	     col.add(DataSet.ColTypes.NUMBER);
 	     col.add(institutions.name);
 	     cols.add(col);
-                }
+        }
 	return cols;
     }
 
 
     public List<List> getData() {
-
 	List<List> data = new ArrayList<List>();
 
-                int to_c = SchoolCalendar.thisCycle();
-                int from_c = to_c - YEARS_REQUIRED + 1;
+        int to_c = SchoolCalendar.thisCycle();
+        int from_c = to_c - YEARS_REQUIRED + 1;
 
-                for (int i = from_c; i <= to_c; i++){
-
-                    List row = new LinkedList();
-                    row.add(SchoolCalendar.cycle2a_y(i));
-                    for (FundingInstitution fund: this.institutions) {
-                         int val = Student.allActive().eq("phd_cycle", Integer.toString(i))
-                         .eq("fundingInstitution", fund).findRowCount();
-                        row.add(new Integer(val));
-                   }
-
-               data.add(row);
-
-               }	
-	
+        for (int i = from_c; i <= to_c; i++){
+            List row = new LinkedList();
+            row.add(SchoolCalendar.cycle2a_y(i));
+            for (FundingInstitution fund: this.institutions) {
+                int val = Student.allActive()
+                    .eq("phd_cycle", Integer.toString(i))
+                    .eq("fundingInstitution", fund)
+                    .findRowCount();
+                row.add(new Integer(val));
+            }
+            data.add(row);
+        }
 	return data;
     }
 
