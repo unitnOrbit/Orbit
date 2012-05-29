c80ec7eeaafeff3be9925369c4f233f26be3c10d
DamianFox <d.fossa.91@gmail.com>
2012-05-29 09:35:19 +0200

    aggiunta di metodi vuoti nei nuovi datasets

---
+++ orbit/app/datasets/AvgMonthAbroadByCycleFor5Cycles.java
@@ -0,0 +1,47 @@
+package datasets;
+
+import java.util.*;
+import models.global.*;
+
+import play.db.ebean.Model;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;  
+import com.avaje.ebean.annotation.Sql;  
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.RawSqlBuilder;
+
+/**
+ * Class for building the statistic representing
+ * Students grouped by nationality.
+ * 
+ * Builds the data table as follows:
+ *  Country | Students
+ * [string] | [number]
+ * ---------+----------
+ *   Italy  |   num    
+ *  Austria |   num
+ *   ...        ...
+ *
+ */
+public class AvgMonthAbroadByCycleFor5Cycles
+    implements DataSet {
+
+    /**
+     * No options supported, does nothing.
+     */
+    public void setOptions(Map options) {
+	// no option supported yet
+    }
+	
+    public List<List<String>> getColumns() {
+	List<List<String>> cols = new LinkedList<List<String>>();
+
+	return cols;
+    }
+ 
+    public List<List> getData() {
+	List<List> data = new ArrayList<List>();
+	return data;
+    }
+
+}
