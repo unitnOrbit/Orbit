73e61c84189e3f6bb0acc305049ba49b2dd7b438
frankyR <lord_vikernes@hotmail.it>
2012-05-29 19:21:16 +0200

    inizio AvgMonthAbroad

--- orbit/app/datasets/AvgMonthAbroadByCycleFor5Cycles.java
+++ orbit/app/datasets/AvgMonthAbroadByCycleFor5Cycles.java
@@ -10,6 +10,9 @@ import com.avaje.ebean.annotation.Sql;
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.RawSqlBuilder;
 
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+
 /**
  * Class for building the statistic representing
  * Students grouped by nationality.
@@ -26,6 +29,19 @@ import com.avaje.ebean.RawSqlBuilder;
 public class AvgMonthAbroadByCycleFor5Cycles
     implements DataSet {
 
+
+    public static final int YEARS_REQUIRED = 5;
+
+    private List<Trip> trips;
+
+    public AvgMonthAbroadByCycleFor5Cycles() {
+        trips = new LinkedList();
+        for (Trip t: Trip.find.all()) {
+            trips.add(t);
+        }
+    }
+
+
     /**
      * No options supported, does nothing.
      */
@@ -35,13 +51,37 @@ public class AvgMonthAbroadByCycleFor5Cycles
 	
     public List<List<String>> getColumns() {
 	List<List<String>> cols = new LinkedList<List<String>>();
+                List<String> col;
+	col = new LinkedList<String>();
+	col.add(DataSet.ColTypes.STRING);
+	col.add("Cycle");
+	cols.add(col);
+	  
+	col = new LinkedList<String>();
+	col.add(DataSet.ColTypes.STRING);
+	col.add("Avg months spent abroad");
+	cols.add(col);
 
 	return cols;
     }
  
     public List<List> getData() {
-	List<List> data = new ArrayList<List>();
-	return data;
-    }
+    	List<List> data = new ArrayList<List>();
 
+                int to_c = SchoolCalendar.thisCycle();
+                int from_c = to_c - YEARS_REQUIRED + 1;
+
+                /*for (int i = from_c; i <= to_c; i++){
+
+                    List row = new LinkedList();
+                    row.add(SchoolCalendar.cycle2a_y(i));
+                    for (Trip tr: this.trips) {                    
+                   }
+                
+                data.add(row);
+
+                }*/
+
+	return data;
+     }
 }
